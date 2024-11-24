49e62baa70c7c55b890b6c02a2dc61950b52a36d
gillianzili <ziliang.guo127@gmail.com>
2024-11-24 15:53:19 -0800

    plot bar chart

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,6 @@
 from pandas import *
+import matplotlib.pyplot as plt
+import bisect
 
 data_edge=read_csv('musae_git_edges.csv')
 data_node=read_csv('musae_git_target.csv')
@@ -14,6 +16,37 @@ for i in range(len(edge_from)):
     v = edge_to[i]
     w = 1
     graph[u].append((v,w))
+
+#create bar chart of numbers of followers
+nums_of_followers=[]
+for i in range(len(graph)):
+    nums_of_followers.append(len(graph[i]))
+nums_of_followers.sort()
+
+# use binary search to fasten the calculation
+bar_categories=[0,20,30,40,60,80,100,500,1000]
+bar_value=[0]*len(bar_categories)
+for num in nums_of_followers:
+    idx = bisect.bisect_left(bar_categories, num)  
+    if idx == len(bar_categories): 
+        bar_value[-1] += 1
+    else:
+        bar_value[idx] += 1
+
+#plot bar chart
+bar_labels = [
+    f"{bar_categories[i]}-{bar_categories[i+1]-1}" if i < len(bar_categories)-1 else f"{bar_categories[i]}+"
+    for i in range(len(bar_categories))
+]
+plt.bar(range(len(bar_categories)), bar_value, color='skyblue', align='center')  
+plt.title('Follower Distribution')
+plt.xlabel('Follower Categories')
+plt.ylabel('Counts')
+plt.ylim(0, 10000)
+plt.xticks(range(len(bar_categories)), bar_labels, rotation=45)  
+plt.tight_layout() 
+plt.show()
+
 
 
 
--- graph_main_function.py
+++ graph_main_function.py
@@ -20,15 +20,31 @@ def dijkstra(graph, source):
     
     return dist
 
-def average_shortest_path_from_source(graph, source):
-    distances = dijkstra(graph, source) 
+
+def floyd_warshall(graph, n):
+    dist = [[float('inf')] * n for _ in range(n)]
+    for u in range(n):
+        dist[u][u] = 0
+        for v, weight in graph[u]:
+            dist[u][v] = weight
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    
+    return dist
+
+
+def average_shortest_path_from_source(graph, source,func):
+    distances = func(graph, source) 
     valid_distances = [d for d in distances if d < float('inf')]
     if valid_distances:  
         return sum(valid_distances) / len(valid_distances)
     return None  
 
+
 def main():
-    print(average_shortest_path_from_source(graph,0))
+    print(average_shortest_path_from_source(graph,27803,dijkstra))
 
 main()
 
